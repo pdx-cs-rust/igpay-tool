d9fb4878df816b5c5d0d7e09bf0a66d6f82e1d2d
Bart Massey <bart.massey@gmail.com>
2022-01-19 18:33:20 -0800

    took rustfmt suggestions

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,11 @@ mod makepig;
 /// assert_eq!("uStDaY", studly_pig("dust"));
 /// ```
 pub fn studly_pig(text: &str) -> String {
-    makepig::make_pig().text_to_pig_latin(text)
+    makepig::make_pig()
+        .text_to_pig_latin(text)
         .chars()
         .enumerate()
-        .flat_map(|(i, ch)| -> Box<dyn Iterator<Item=char>> {
+        .flat_map(|(i, ch)| -> Box<dyn Iterator<Item = char>> {
             if i % 2 == 1 {
                 Box::new(ch.to_uppercase())
             } else {
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use makepig::*;
 
 fn main() {
     let pig = make_pig();
-    let pigtext=pig.text_to_pig_latin("Can't touch this! Awoo-away!");
+    let pigtext = pig.text_to_pig_latin("Can't touch this! Awoo-away!");
     println!("{}", pigtext);
 }
