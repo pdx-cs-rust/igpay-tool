d66f4e22c8d1dfbe9ff7bdf1437d8bfbc830e7e3
Bart Massey <bart.massey@gmail.com>
2022-01-19 18:11:45 -0800

    functionalized function

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,15 @@ mod makepig;
 /// assert_eq!("uStDaY", studly_pig("dust"));
 /// ```
 pub fn studly_pig(text: &str) -> String {
-    let pig = makepig::make_pig();
-    let pigged = pig.text_to_pig_latin(text);
-    let mut result = String::new();
-    let mut odd = false;
-    for ch in pigged.chars() {
-        if odd {
-            let uch = ch.to_uppercase();
-            result.extend(uch);
-        } else {
-            result.push(ch);
-        }
-        odd = !odd;
-    }
-    result
+    makepig::make_pig().text_to_pig_latin(text)
+        .chars()
+        .enumerate()
+        .flat_map(|(i, ch)| -> Box<dyn Iterator<Item=char>> {
+            if i % 2 == 1 {
+                Box::new(ch.to_uppercase())
+            } else {
+                Box::new(std::iter::once(ch))
+            }
+        })
+        .collect()
 }
